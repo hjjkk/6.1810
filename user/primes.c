cb9a2356a8bb489a68824d0e239ce2f1ee9f721c
hjjkk <kongyf1999@163.com>
2023-05-15 23:45:25 +0800

    fix: change saving-array to stream-sending number

--- user/primes.c
+++ user/primes.c
@@ -6,34 +6,26 @@ const uint MAX_NUM = 35;
 // pipe(p)
 // p[0] used for read while p[1] used for write
 
-int solve(int neighbor_fd) {
-    // recv from left , send to right
-    int prime_num;
-    int a[MAX_NUM];
-    memset(a, 0, sizeof a);
-    read(neighbor_fd, &prime_num, sizeof prime_num);
-    if (prime_num == 0) return 0;
-    printf("prime %d\n", prime_num);
-    int recv = -1;
+void solve(int neighbor_fd) {
+    int prime, recv;
+    read(neighbor_fd, &prime, sizeof prime);
+    if (prime == 0) 
+      return ;
+    printf("prime %d\n", prime);
+
     int p[2];
     pipe(p);
-    int ret;
-    while ((ret = read(neighbor_fd, &recv, sizeof recv) != 0) && (recv <= MAX_NUM)) {
-        if (recv % prime_num != 0) {
-          a[recv] = 1;
+    while ((read(neighbor_fd, &recv, sizeof recv) != 0) && (recv <= MAX_NUM)) {
+        if (recv % prime != 0) {
+           write(p[1], &recv, sizeof recv);
         }
     }
-
     close(neighbor_fd);
-   
-    for (int i = 2; i <= MAX_NUM; i ++) {
-      if (a[i]) {
-        write(p[1], &i, sizeof i);
-      }
-    }
     close(p[1]);
+
+    // next solve
     solve(p[0]);
-    return 0;
+    return ;
 }
 
 int
@@ -41,16 +33,14 @@ main(int argc, char *argv[])
 {
   int p[2];
   pipe(p);
-
   if (fork() == 0) {
     solve(p[0]);
   } else { 
-    int i;
-    for (i = 2; i <= MAX_NUM + 1; i ++) {
+    for (int i = 2; i <= MAX_NUM + 1; i ++) { // 多发一个判断停止
       write(p[1], &i, sizeof i);
     }
+    close(p[1]);
     wait(0);
   }
-  close(p[1]);
   exit(0);
 }
